841fcb18d0aeef8fc32ac846a2c548ff02306213
Addison <addison@macbook-air-2.lan>
2026-06-09 16:26:37 -0500

    gear completed

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 const overlay = document.querySelector(".overlay");
 const lines = document.querySelector(".lines");
 const gear = document.querySelector("#gear");
-const lines2 = document.querySelector("#lines");
-
+const education = document.querySelector("#Education");
+const home = document.querySelector("#Home");
+const engineering = document.querySelector("#Engineering");
+const experience = document.querySelector("#Experience");
 
 
 
 gear.addEventListener("click", () => {
     overlay.classList.add("show");
     lines.classList.add("active");
+    gear.classList.add("cursor");
     setTimeout(() => {
-        lines2.classList.toggle("fadein");
+        education.classList.add("fadein");
+        home.classList.add("fadein");
+        engineering.classList.add("fadein");
+        experience.classList.add("fadein");
     }, 1200);
     document.body.classList.add('no-scroll');
 });
 
 overlay.addEventListener("click", () => {
-    lines2.classList.remove("fadein");
+    education.classList.remove("fadein");
+    home.classList.remove("fadein");
+    engineering.classList.remove("fadein");
+    experience.classList.remove("fadein");
 
     setTimeout(() => {
         overlay.classList.remove("show");
         lines.classList.remove("active");
+        gear.classList.remove("cursor");
     }, 1000);
     document.body.classList.remove('no-scroll');
 });
 
 
-
-
+education.addEventListener("click", () => {
+    window.location.href = "education.html";
+});
